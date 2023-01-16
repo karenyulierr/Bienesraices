e86d0dc47afc8cf601bf39cbafc5caa69cba458c
Karenyulier <karenyulierr@gmail.com>
2023-01-15 22:01:54 -0500

    Create

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -10,7 +10,7 @@
     <p class="copyright">Todos los derechos Reservados <?php echo date('Y'); ?> &copy;</p>
 </footer>
 
-<script src="../Bienesraices/build/js/bundle.min.js"></script>
+<script src="/build/js/bundle.min.js"></script>
 </body>
 
 </html>
